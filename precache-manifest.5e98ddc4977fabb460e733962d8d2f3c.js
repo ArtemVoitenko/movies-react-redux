d294cc374ee02ea7d2f4a5bd30c6b2ef2578994c
unknown <artvoitenk072@gmail.com>
2019-07-18 16:07:26 +0300

    Updates

---
+++ precache-manifest.5e98ddc4977fabb460e733962d8d2f3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8120011068afe0ea41ae9eec3fbe651f",
+    "url": "/movies-react-redux/index.html"
+  },
+  {
+    "revision": "26d0a544efa54b3a591a",
+    "url": "/movies-react-redux/static/css/main.41b95c81.chunk.css"
+  },
+  {
+    "revision": "91d0e4c2b81aa738fdbd",
+    "url": "/movies-react-redux/static/js/2.091e207d.chunk.js"
+  },
+  {
+    "revision": "26d0a544efa54b3a591a",
+    "url": "/movies-react-redux/static/js/main.94d51947.chunk.js"
+  },
+  {
+    "revision": "2343af3cb1ab26b44c29",
+    "url": "/movies-react-redux/static/js/runtime~main.fbb59805.js"
+  },
+  {
+    "revision": "0062c34665a3fc0f2278cd4e955702ec",
+    "url": "/movies-react-redux/static/media/OpenSans-Bold.0062c346.ttf"
+  },
+  {
+    "revision": "5a798cdadc7cd321e3f72425b70bface",
+    "url": "/movies-react-redux/static/media/OpenSans-Regular.5a798cda.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.10ef59a52b0af4b192cdef3cf9c98b06.js"
+  "/movies-react-redux/precache-manifest.5e98ddc4977fabb460e733962d8d2f3c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movies-react-redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
