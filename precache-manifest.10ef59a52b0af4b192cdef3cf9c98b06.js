d30b7d2bec6454f8d0d2b3c1603aa2689145034e
unknown <artvoitenk072@gmail.com>
2019-07-18 16:03:12 +0300

    Updates

---
+++ precache-manifest.10ef59a52b0af4b192cdef3cf9c98b06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1264f9df9f3e19caeb04b136175b2c7e",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "2ea07f78b90279aceca6",
+    "url": "/react-gh-pages/static/css/main.53264119.chunk.css"
+  },
+  {
+    "revision": "14df52894ebedf671bd1",
+    "url": "/react-gh-pages/static/js/2.8bcd684c.chunk.js"
+  },
+  {
+    "revision": "2ea07f78b90279aceca6",
+    "url": "/react-gh-pages/static/js/main.94d51947.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "0062c34665a3fc0f2278cd4e955702ec",
+    "url": "/react-gh-pages/static/media/OpenSans-Bold.0062c346.ttf"
+  },
+  {
+    "revision": "5a798cdadc7cd321e3f72425b70bface",
+    "url": "/react-gh-pages/static/media/OpenSans-Regular.5a798cda.ttf"
+  }
+]);
